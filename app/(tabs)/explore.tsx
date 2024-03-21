4fb721bd5575bad046d2ab7e50396b5990a49446
frustrated018 <niloyd018@gmail.com>
2024-03-21 12:51:06 +0600

    intial commit
    setup expo router with tabs template and starting to work on the home page

--- app/(tabs)/explore.tsx
+++ app/(tabs)/explore.tsx
@@ -1,12 +1,12 @@
 import { View, Text } from "react-native";
 import React from "react";
 
-const Page = () => {
+const Explore = () => {
   return (
     <View>
-      <Text>The home page </Text>
+      <Text>This will be the explore page</Text>
     </View>
   );
 };
 
-export default Page;
+export default Explore;
