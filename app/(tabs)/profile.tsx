51a68825e2b789b94e1bfdba12b95b66c0448c7e
frustrated018 <niloyd018@gmail.com>
2024-03-22 19:42:06 +0600

    Added authentication logic
    Users will be redirected to the login page if they are not logged in
    And if they choose not to login and close the modal then they can still login through the profile page

--- app/(tabs)/profile.tsx
+++ app/(tabs)/profile.tsx
@@ -1,12 +1,24 @@
-import { View, Text } from 'react-native'
-import React from 'react'
+import { View, Text, Button } from "react-native";
+import React from "react";
+import { useAuth } from "@clerk/clerk-expo";
+import { useRouter } from "expo-router";
 
 const Profile = () => {
+  const { signOut, isSignedIn } = useAuth();
+  const router = useRouter();
+
   return (
     <View>
       <Text>Profile Page</Text>
+      <Button title="Sign Out" onPress={() => signOut()} />
+      {!isSignedIn && (
+        <Button
+          title="Sign In"
+          onPress={() => router.push("/(modals)/login")}
+        />
+      )}
     </View>
-  )
-}
+  );
+};
 
-export default Profile
+export default Profile;
--- app/_layout.tsx
+++ app/_layout.tsx
@@ -1,7 +1,29 @@
 import { useFonts } from "expo-font";
-import { Stack } from "expo-router";
+import { Stack, useRouter } from "expo-router";
 import * as SplashScreen from "expo-splash-screen";
 import { useEffect } from "react";
+import * as SecureStore from "expo-secure-store";
+import { ClerkProvider, useAuth } from "@clerk/clerk-expo";
+
+const CLERK_PUBLISHABLE_KEY = process.env.EXPO_PUBLIC_CLERK_PUBLISHABLE_KEY;
+
+const tokenCache = {
+  async getToken(key: string) {
+    try {
+      return SecureStore.getItemAsync(key);
+    } catch (error) {
+      return null;
+    }
+  },
+
+  async saveToken(key: string, value: string) {
+    try {
+      return SecureStore.setItemAsync(key, value);
+    } catch (error) {
+      return;
+    }
+  },
+};
 
 export {
   // Catch any errors thrown by the Layout component.
@@ -38,10 +60,26 @@ export default function RootLayout() {
     return null;
   }
 
-  return <RootLayoutNav />;
+  return (
+    <ClerkProvider
+      publishableKey={CLERK_PUBLISHABLE_KEY!}
+      tokenCache={tokenCache}
+    >
+      <RootLayoutNav />
+    </ClerkProvider>
+  );
 }
 
 function RootLayoutNav() {
+  const { isLoaded, isSignedIn } = useAuth();
+  const router = useRouter();
+
+  useEffect(() => {
+    if (isLoaded && !isSignedIn) {
+      router.push("/(modals)/login");
+    }
+  }, [isLoaded]);
+
   return (
     <Stack>
       <Stack.Screen name="(tabs)" options={{ headerShown: false }} />
