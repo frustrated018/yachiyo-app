9a11dabccb5a8a49f2904479712a7d852ec2b1ef
frustrated018 <niloyd018@gmail.com>
2024-03-25 10:22:47 +0600

    Added animation to the cards loading

--- components/Listings.tsx
+++ components/Listings.tsx
@@ -12,6 +12,7 @@ import { defaultStyles } from "@/constants/Styles";
 import { Link } from "expo-router";
 import { Listing } from "@/interfaces/listings";
 import { Ionicons } from "@expo/vector-icons";
+import Animated, { FadeInRight, FadeOutLeft } from "react-native-reanimated";
 
 interface Props {
   listings: any[];
@@ -41,44 +42,46 @@ const Listings = ({ listings: items, category }: Props) => {
   // Render one listing row for the FlatList
   const renderRow: ListRenderItem<Listing> = ({ item }) => {
     return (
-      <View>
-        <Link href={`/listing/${item.id}`} asChild>
-          <TouchableOpacity>
-            <View style={styles.listing}>
-              <Image source={{ uri: item.medium_url }} style={styles.image} />
+      <Link href={`/listing/${item.id}`} asChild>
+        <TouchableOpacity>
+          <Animated.View
+            style={styles.listing}
+            entering={FadeInRight}
+            exiting={FadeOutLeft}
+          >
+            <Image source={{ uri: item.medium_url }} style={styles.image} />
 
-              <TouchableOpacity
-                style={{ position: "absolute", right: 30, top: 30 }}
-              >
-                <Ionicons name="heart-outline" size={24} color="#000" />
-              </TouchableOpacity>
+            <TouchableOpacity
+              style={{ position: "absolute", right: 30, top: 30 }}
+            >
+              <Ionicons name="heart-outline" size={24} color="#000" />
+            </TouchableOpacity>
 
-              {/* Text */}
-              <View
-                style={{
-                  flexDirection: "row",
-                  justifyContent: "space-between",
-                }}
-              >
-                <Text style={{ fontSize: 16, fontFamily: "mon-sb" }}>
-                  {item.name}
-                </Text>
-                <View style={{ flexDirection: "row", gap: 4 }}>
-                  <Ionicons name="star" size={16} />
-                  <Text style={{ fontFamily: "mon-sb" }}>
-                    {item.review_scores_rating / 20}
-                  </Text>
-                </View>
-              </View>
-              <Text style={{ fontFamily: "mon" }}>{item.room_type}</Text>
+            {/* Text */}
+            <View
+              style={{
+                flexDirection: "row",
+                justifyContent: "space-between",
+              }}
+            >
+              <Text style={{ fontSize: 16, fontFamily: "mon-sb" }}>
+                {item.name}
+              </Text>
               <View style={{ flexDirection: "row", gap: 4 }}>
-                <Text style={{ fontFamily: "mon-sb" }}>$ {item.price}</Text>
-                <Text style={{ fontFamily: "mon" }}>night</Text>
+                <Ionicons name="star" size={16} />
+                <Text style={{ fontFamily: "mon-sb" }}>
+                  {item.review_scores_rating / 20}
+                </Text>
               </View>
             </View>
-          </TouchableOpacity>
-        </Link>
-      </View>
+            <Text style={{ fontFamily: "mon" }}>{item.room_type}</Text>
+            <View style={{ flexDirection: "row", gap: 4 }}>
+              <Text style={{ fontFamily: "mon-sb" }}>$ {item.price}</Text>
+              <Text style={{ fontFamily: "mon" }}>night</Text>
+            </View>
+          </Animated.View>
+        </TouchableOpacity>
+      </Link>
     );
   };
   return (
@@ -95,8 +98,8 @@ const Listings = ({ listings: items, category }: Props) => {
 const styles = StyleSheet.create({
   listing: {
     padding: 16,
-    gap:10,
-    paddingVertical:16
+    gap: 10,
+    paddingVertical: 16,
   },
   image: {
     width: "100%",
